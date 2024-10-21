bf15bc9773f65852e338508c40a928190bfa6190
Ezequiel <ki3lmigu3l@gmail.com>
2024-10-20 22:17:36 -0300

    Criação inicial do projeto com dependências do JPA

---
+++ jpa/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.github.ki3lmigu3l;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
